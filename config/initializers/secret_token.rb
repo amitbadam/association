9b0e27e2773d40355f48b713ccb88746a2b22db3
amitbadam <amitbadam@gmail.com>
2013-11-23 00:13:14 +0530

    simple association

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Association::Application.config.secret_token = '51c392cffae52d5df1b09eafa42eb4169939df779770cf5edcf863ac1c5f4a3e2efad27cb7b5dcc68503f07b73898e1d0b2de1a566e37511f2d9f8b3356232f6'
